4358ade62fdb4ad65bd6cab22ec05bb74ecb34b0
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-04-27 20:41:10 -0300

    hashing password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcrypt';
 
 class User extends Model {
   static init(sequelize) {
@@ -15,6 +16,11 @@ class User extends Model {
         sequelize,
       }
     );
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
     return this;
   }
 }
