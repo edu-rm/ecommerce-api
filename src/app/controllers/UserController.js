1ca7b68afdef37f96f55917e7b98423b42daf509
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-04-27 20:36:51 -0300

    Schema validation on user creation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,24 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+      cpf: Yup.string().required(),
+      birth: Yup.string().required(),
+    });
+
+    /**
+     * Verificando se o body bate com o schema
+     */
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'A validação falhou' });
+    }
+
     const newUser = await User.create(req.body);
 
     return res.json(newUser);
